4567d3df3dd9c6654c143e7be5c03a17cbfb04a6
Yasir_Hassan <yasir.hassan7722@gmail.com>
2023-01-21 01:36:53 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { GitHubService } from './gethub.service';
+import { GitHubUser } from './githubuser';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'github-search-service';
+import { filter,debounceTime,distinctUntilChanged } from 'rxjs/operators';
+
+@Component({ 
+	selector: 'app-root', 
+	templateUrl: './app.component.html',  
+	providers: [GitHubService] }) 
+
+export class AppComponent implements OnInit {
+	 searchControl = new FormControl();  
+	 users: GitHubUser[] = [];
+	 constructor(private _githubService: GitHubService){} 
+
+	ngOnInit() { 
+	this.searchControl.valueChanges 
+	    .pipe(filter(text => text.length >= 3), debounceTime(400),distinctUntilChanged())                
+          .subscribe(value => { this._githubService.getGitHubData(value) 
+                                    .subscribe(data => {this.users = data.items;});                             
+     });          
+  } 
 }
+
+  
+  
